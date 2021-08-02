fbbec0f6773f225019f75aa02a7a5e19be324830
Utkarsh Karaiya <karaiyautkarsh@gmail.com>
2021-08-03 03:35:46 +0530

    Emplemented module 2 (loops,ifelse etc) in Vue_js

--- Module 1/app.js
+++ Module 1/app.js
@@ -1,8 +1,24 @@
 Vue.createApp({
 data(){
     return {
-      result:'Hello World!!'  
-    }
+      result:'Hello World!!',
+      name:'This Is Utkarsh!!',
+      flag:false,
+      arr:[1,2,3,4,5] 
+    }},
+    methods:{
+       fullname(){
+            return `${this.result} ${this.name}`
+       }
+    
 }
 
 }).mount('#root')
+
+// Vue.createApp({
+//   data(){
+//     return {
+//       name:"utkarsh"
+//     }
+//   }
+// }).mount("#root1")
